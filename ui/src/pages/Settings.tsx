c179acce49d6e1608139dbb347bb077478c8ab3d
pfa230 <pfa230@pfa.name>
2026-06-16 16:07:49 -0400

    feat(ui): compose Settings & Printers page (#23)
    Fixes #23

--- ui/src/pages/Settings.tsx
+++ ui/src/pages/Settings.tsx
@@ -1,7 +1,12 @@
+import { SettingsSection } from "./settings/SettingsSection";
+import { PrintersSection } from "./settings/PrintersSection";
+
 export function Settings() {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <h1 className="text-2xl font-semibold">Settings</h1>
+      <SettingsSection />
+      <PrintersSection />
     </div>
   );
 }
